c01d34ea7655659ed10decac3e1f15d8cb0fe760
ptrjs <peterjose64@gmail.com>
2023-09-22 16:40:24 +0700

    func

---
+++ phase0/part7.5/2.js
@@ -0,0 +1,16 @@
+// Tugas 2
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+let num1 = 7
+let num2 = 5
+
+
+function calculateMultiply(num1,num2){
+  return num1 * num2
+}
+
+let hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
