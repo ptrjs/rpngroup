6028566326f236e481c31ef0b3c19a28e8ceff18
ptrjs <peterjose64@gmail.com>
2023-09-22 16:29:02 +0700

    loop and func

---
+++ phase0/part6/looping.js
@@ -0,0 +1,8 @@
+for(let i = 0 ; i<3; i++){
+    for(let j = 0 ; j<10;j++){
+        console.log("isi dari j: " +j+ " iterasi ke-"+i)
+    }
+    console.log("")
+}
+
+// j akan menghasilkan angka 0-9. dengan pengulangan sebanyak yang sudah didefinisikan di i yaitu 0-2
