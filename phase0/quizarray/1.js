6924da4083e8f3485321a9f8c6805a2ce5026808
ptrjs <peterjose64@gmail.com>
2023-10-06 19:39:59 +0700

    1 dipersingkat, 3 revisi

--- phase0/quizarray/1.js
+++ phase0/quizarray/1.js
@@ -4,6 +4,44 @@ Function akan me-return jarak spasi antar karakter 'o' dengan karakter 'x' yang 
 Contoh, jika arr adalah ['x', ' ', 'o', ' ', ' ', 'x'], maka jarak terdekat dari 'o' ke 'x' adalah 2. 
 Jika tidak ditemukan 'x' sama sekali, function akan me-return nilai 0.
 */
+
+//hasil revisi 
+function targetTerdekat(arr) {
+  // you can only write your code here!
+  //deklarasi variabel untuk mendapatkan index x dan o
+  let o = -1; //-1 kalau o ga ketemu dan > -1 kalau o ketemu pada array
+  let x = -1;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 'o') {
+      o = i
+      console.log("o"+o)
+    } else if (arr[i] === 'x') {
+      x = i
+      console.log("x"+x)
+    }
+    if (o >= 0 && x >= 0) {
+      //kalau o dan x dah ketemu ya berhentiin aja
+      break;
+    }
+  }
+
+  if (o === -1 || x === -1) {
+    return 0
+  } else {
+    return Math.abs(o - x) //math abs biar kalkulasinya tetap di angka positif
+  }
+}
+
+// TEST CASES
+//console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+//console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+//console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+//console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+
+/* hasil salah
+
 function targetTerdekat(arr) {
     // you can only write your code here!
     let tmp1=0
@@ -66,3 +104,5 @@ function targetTerdekat(arr) {
   console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
   console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
   console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+
+  */
--- phase0/quizarray/3.js
+++ phase0/quizarray/3.js
@@ -3,6 +3,40 @@ diberikan sebuah function groupAnimals(animals) yang menerima satu parameter ber
 fungsi ini akan me-return array 2 dimensi
 */
 function groupAnimals(animals) {
+
+    let groupA = []
+    let groupC = []
+    let groupK = []
+    let groupU = []
+
+    for(let i = 0 ; i < animals.length; i++){
+        if(animals[i][0]=='a'){
+            groupA.push(animals[i])   
+        }else if(animals[i][0]=='c'){
+            groupC.push(animals[i])   
+        }else if(animals[i][0]=='k'){
+            groupK.push(animals[i])   
+        }else if(animals[i][0]=='u'){
+            groupU.push(animals[i])   
+        }
+       
+    }
+
+    return [groupA, groupC, groupK, groupU]
+}
+  
+  
+  // TEST CASES
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+
+
+
+  /* salah 
+  
+  function groupAnimals(animals) {
     // you can only write your code here!
    
     let groups = [];
@@ -28,8 +62,5 @@ function groupAnimals(animals) {
  
   
   
-  // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+  
+  */
