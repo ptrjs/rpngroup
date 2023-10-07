cc86c6953893f942cef03901ba941318a04f5a98
ptrjs <peterjose64@gmail.com>
2023-10-08 00:36:55 +0700

    a

--- phase0/part9.2/5.js
+++ phase0/part9.2/5.js
@@ -24,7 +24,7 @@ function ubahHuruf(kata) {
   }
   
   // TEST CASES
-  console.log(ubahHuruf('wow')); // xpx
+ console.log(ubahHuruf('wow')); // xpx
  console.log(ubahHuruf('developer')); // efwfmpqfs
  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
  console.log(ubahHuruf('keren')); // lfsfo
